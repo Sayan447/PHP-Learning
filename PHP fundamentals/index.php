bc637bcb9a1770319d17f53df8be5540167c634a
Sayan Chakraborty <69922781+Sayan447@users.noreply.github.com>
2026-03-17 16:35:53 +0530

    Add files via upload

---
+++ PHP fundamentals/index.php
@@ -0,0 +1,32 @@
+<?php 
+// echo 'Sayan';
+// $x = 5;
+// echo  PHP_EOL;  // for new line
+// var_dump($x); 
+
+///////
+
+
+
+// var_dump(5);
+// var_dump("John");
+// var_dump(3.14);
+// var_dump(true);
+// var_dump([2, 3, 56]);
+// var_dump(NULL);
+
+
+// output:- 
+// int(5)
+// string(4) "John"
+// float(3.14)
+// bool(true)
+// array(3) {
+//   [0]=>
+//   int(2)
+//   [1]=>
+//   int(3)
+//   [2]=>
+//   int(56)
+// }
+// NULL
---
+++ PHP fundamentals/variablescooping.php
@@ -0,0 +1,133 @@
+
+
+
+
+<?php
+//  PHP variables can be declared anywhere in the PHP code.
+
+// The scope of a variable is the part of the script where the variable can be referenced/used.
+
+// PHP has three different variable scopes: -->
+
+// <!-- global
+// local
+// static
+// A variable declared outside a function has a GLOBAL SCOPE and can only be accessed outside a function: 
+
+// $x = 5; // global scope
+
+// function myTest() {
+//   // using x inside this function will not work
+// //   echo "Variable x inside function is: $x";
+// }
+// myTest();
+
+// // echo  PHP_EOL . "Variable x outside function is: $x";
+// echo   " Variable x outside function is: $x\n";
+
+
+
+
+// local scope
+// function myTest() {
+//   $x = 5; // local scope
+//   echo "\nVariable x inside function is: $x";
+// }
+// myTest();
+
+// // using x outside the function will not work
+// echo "\nVariable x outside function is: $x";
+
+
+
+
+
+// /////////////// static scoping
+
+// function myTest() {
+//   static $x = 0; // static scope
+//   echo "\n $x";
+//   $x++;
+// }
+
+// myTest();
+// myTest();
+// myTest();
+
+
+
+
+
+
+
+
+
+
+// //////////////////// --- global keyword
+// $x = 15;
+
+// function addTen() {
+//     global $x;
+//     $x += 10;
+// }
+
+// addTen();
+// echo $x; // 15
+
+/////////// superGlobal 
+// $x = 50;
+// function showX() {
+//     echo $GLOBALS['x']; // access global $x without using global keyword
+// }
+
+// showX();
+
+
+
+// $x = 5;
+// $y = 10;
+
+// function myTest() {
+//   return $GLOBALS['x'] + $GLOBALS['y'];
+// }
+
+// $test = myTest();
+// echo $test;
+
+
+
+
+
+//// Indexed or numeric array
+// $colors = array("Red", "Green", "Blue");
+// // or 
+// $colors = ["Red", "Green", "Blue"];
+
+// echo $colors[0];
+
+
+
+
+
+
+
+// $x = 5;
+// var_dump($x);
+
+// $x = "Hello";
+// var_dump($x);
+
+
+
+
+
+
+
+
+
+$x = 5;
+$x = (string) $x;
+var_dump($x);
+
+
+?>
