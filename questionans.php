d03fe69d6cb2bbeca37311b4ecd64937c5df6cea
Sayan Chakraborty <69922781+Sayan447@users.noreply.github.com>
2026-03-17 17:10:06 +0530

    Add files via upload

---
+++ questionans.php
@@ -0,0 +1,16 @@
+
+<?php
+//  <!-- Find the length of a given string --> 
+//  $name = readline("Enter your name: ");
+//  echo $name;
+
+
+
+//  Count total characters excluding spaces
+
+// $count = mb_strlen("Sonu");
+// echo $count;
+$x  = "sonu hello";
+$y = str_replace( " ","",$x);
+echo ($y); 
+// echo strlen($y);
