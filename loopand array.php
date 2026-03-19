fa8be416f50e9f719889f584cbb8ac5e36c7d148
Sayan Chakraborty <69922781+Sayan447@users.noreply.github.com>
2026-03-19 17:18:00 +0530

    Add files via upload

---
+++ loopand array.php
@@ -0,0 +1,55 @@
+<?php
+// $car = array("brand" => "Ford" , "Model"=> 'Mustang',"Year"=>1964);
+
+// // foreach ($car as $x => $y){
+// //     echo "$x :  $y";
+// // };
+
+// echo var_dump($car);
+
+
+// $a = [1, 2];
+// $b = [3, 4];
+// print_r(array_merge($a, $b));
+
+
+
+
+
+
+// $arr = ["apple", "banana"];
+// echo var_dump(in_array("apple", $arr)); // 1 (true)
+
+
+
+
+
+
+// $arr = ["a" => 1, "b" => 2];
+// print_r(array_keys($arr));
+// print_r(array_values($arr));
+
+
+// $arr = [3, 1, 2];
+// echo sort($arr);
+// print_r($arr);
+
+
+
+
+
+
+
+
+
+
+
+// $str = "a,b,c";
+// print_r(explode(",", $str));
+
+
+
+
+
+// $arr = ["a", "b", "c"];
+// echo implode(",", $arr); // a,b,c
